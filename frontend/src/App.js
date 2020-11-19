b0f6fb8c65a3fb3f89045c0d73e5f6dffefa87bb
Drayerr <43964939+Drayerr@users.noreply.github.com>
2020-11-19 20:54:00 -0300

    Mobile
    Mobile started

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+//CSS
+import './global.css'
+
+//COMPONENTS
+import Routes from './routes'
+
+function App() {
+
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
